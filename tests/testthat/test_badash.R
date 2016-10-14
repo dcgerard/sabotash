379d77fa8a28ef516b9618d655c03d0774300e64
David Gerard <gerard.1787@gmail.com>
2016-10-14 15:44:27 -0500

    added more tests

--- tests/testthat/test_badash.R
+++ tests/testthat/test_badash.R
@@ -4,6 +4,11 @@ test_that("bad_ash runs", {
     betahat   <- stats::rnorm(100)
     sebetahat <- stats::rgamma(100, 1, 1)
 
-    bout <- bad_ash(betahat = betahat, sebetahat = sebetahat)
+    bout1 <- bad_ash(betahat = betahat, sebetahat = sebetahat)
+    bout2 <- bad_ash(betahat = betahat, sebetahat = sebetahat, df = 2)
+    bout3 <- bad_ash(betahat = betahat, sebetahat = sebetahat, df = 2, mixcompdist = "halfuniform")
+    bout4 <- bad_ash(betahat = betahat, sebetahat = sebetahat, df = 2, mixcompdist = "normal")
+    bout5 <- bad_ash(betahat = betahat, sebetahat = sebetahat, df = 2, mixcompdist = "+uniform")
+    bout6 <- bad_ash(betahat = betahat, sebetahat = sebetahat, df = 2, mixcompdist = "-uniform")
 }
 )
