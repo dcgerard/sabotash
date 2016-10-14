a00e7f2f04700c27d53e2c1f5abce9ba5f93714f
David Gerard <gerard.1787@gmail.com>
2016-10-14 16:28:37 -0500

    changed zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,17 @@
 .onLoad <- function(libname, pkgname) {
     ## The evil part ------------------------------------------------------------
     suppressWarnings({
+        require(ashr)
+        unlockBinding("ash", as.environment("package:ashr"))
         utils::assignInNamespace("ash", value = bad_ash, ns = "ashr",
                                  envir = as.environment("package:ashr"))
+        assign("ash", bad_ash, as.environment("package:ashr"))
+        lockBinding("ash", as.environment("package:ashr"))
+
+        unlockBinding("ash.workhorse", as.environment("package:ashr"))
         utils::assignInNamespace("ash.workhorse", value = bad_ash.workhorse, ns = "ashr",
                                  envir = as.environment("package:ashr"))
+        assign("ash.workhorse", bad_ash.workhorse, as.environment("package:ashr"))
+        lockBinding("ash.workhorse", as.environment("package:ashr"))
     })
 }
